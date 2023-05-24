b749829afa2963eaa169079869f7d0a09b082dc0
Parth Gupta <95039857+parthgupta0824@users.noreply.github.com>
2023-05-25 03:16:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+        colors: {
+        'facebook-blue' : '#1877f2',
+        'facebook-background-blue' : '#F0F2F5',
+        'facebook-green-button' :'#42b72a',
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
